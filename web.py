9a75ef5f4571934d3be404d0b216c9cecd365e51
Sahil Singh <116958178+SahilSingh3410@users.noreply.github.com>
2024-07-22 11:18:32 +0530

    initial commit

---
+++ web.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import re
+
+st.set_page_config(layout="wide")
+
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+st.title('Fake News Detection System')
+
+# function to process the text
+def textFormatter(text):
+    text = text.lower()  # convert string into lowercase
+    text = re.sub(r'https?://\S+|www\.\S+','',text)   # remove urls
+    text = re.sub(r'<.*?>','',text)    #remove HTML tags
+    text = re.sub(r'[^\W\S]','',text)   #remove punctuation
+    text = re.sub(r'\d', '',text)       #remove digits
+    text = re.sub(r'\n', '',text)    #remove newline characters
+
+    return text
+
+
+def manual_tester(news):
+    temp = {"text" : [news]}
+    x_news = pd.DataFrame(temp)
+    x_news["text"] = x_news["text"].apply(textFormatter)
+    xv_news = vectorizer.transform(x_news['text'])
+    prediction = model.predict(xv_news)
+
+    return prediction[0]
+
+
+news = st.text_area(label = '', placeholder= "Enter the news")
+
+if st.button('Check'):
+    result = manual_tester(news)
+
+    if result == 1:
+        st.success("Real News")
+    else:
+        st.warning('Fake News')
